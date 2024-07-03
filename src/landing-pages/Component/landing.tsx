5739d8b787e2167376bdb51af76f1a5570606ad5
LilBoii <choreiwken@outlook.com>
2024-07-03 14:48:32 +0700

    After change to bootstrap

--- src/landing-pages/Component/landing.tsx
+++ src/landing-pages/Component/landing.tsx
@@ -12,45 +12,82 @@ const LandingScreen: React.FC = () => {
       transition={{ duration: 2 }}
     >
       <center>
-        <div className="landing-page-text">
-          We Don`t Jump <br />
+        <div className="landing-page-text mt-xl-5">
+          Don`t Jump <br />
           We <span className="text-blue">Hops!</span>
         </div>
-        <div className="btn landing-page-content mt-5">
+        <div className="btn landing-page-content mt-xl-2">
           <a href="">Connect World</a>
         </div>
       </center>
-      <div className="corner top-left landing-page-logo">
-        <a href="" className="image-glow">
-          <img src="/image/logo.svg" alt="logo" />
-        </a>
+      {/* Start Desktop Section */}
+      <div className="d-mb-none">
+        <div className="corner top-left landing-page-logo">
+          <a href="" className="image-glow">
+            <img src="/image/logo.svg" alt="logo" />
+          </a>
+        </div>
+        <div className="corner bottom-left landing-page-content">
+          <a href="#" className="landing-page-link glow">
+            Hops Story
+          </a>
+          <br />
+          <a
+            className="landing-page-link landing-page-content glow"
+            href="https://docs.google.com/document/d/12flB-zl8DdPh-oXwoK9UIz9oSY6eOK42bqt92Tnn7Y0/edit?usp=sharing"
+            target="_blank"
+          >
+            White Paper
+          </a>
+        </div>
+        <div className="corner bottom-right landing-page-content">
+          <img
+            src="/image/hops-world.svg"
+            alt="hops-world"
+            className="game-logo image-glow"
+          />
+          <img
+            src="/image/hops-play.svg"
+            alt="hops-play"
+            className="game-logo image-glow landing-page-content"
+          />
+        </div>
+        <div className="corner top-right landing-page-content"></div>
       </div>
-      <div className="corner bottom-left landing-page-content">
-        <a href="#" className="landing-page-link glow">
-          Hops Story
-        </a>
-        <br />
-        <a
-          className="landing-page-link landing-page-content glow"
-          href="https://docs.google.com/document/d/12flB-zl8DdPh-oXwoK9UIz9oSY6eOK42bqt92Tnn7Y0/edit?usp=sharing"
-          target="_blank"
-        >
-          White Paper
-        </a>
+      {/* Start Mobile Section */}
+      <div className="d-xl-none">
+        <div className="position-absolute  landing-page-logo">
+          <div className="row">
+            <a href="#" className="landing-page-link glow col-4">
+              Hops Story
+            </a>
+            <a href="" className="image-glow col-4">
+              <img src="/image/logo.svg" alt="logo" />
+            </a>
+            <a
+              className="landing-page-link landing-page-content glow col-4"
+              href="https://docs.google.com/document/d/12flB-zl8DdPh-oXwoK9UIz9oSY6eOK42bqt92Tnn7Y0/edit?usp=sharing"
+              target="_blank"
+            >
+              White Paper
+            </a>
+          </div>
+        </div>
+        <div className="corner bottom-left landing-page-content"></div>
+        <div className="corner bottom-right landing-page-content">
+          <img
+            src="/image/hops-world.svg"
+            alt="hops-world"
+            className="game-logo image-glow"
+          />
+          <img
+            src="/image/hops-play.svg"
+            alt="hops-play"
+            className="game-logo image-glow landing-page-content"
+          />
+        </div>
+        <div className="corner top-right landing-page-content"></div>
       </div>
-      <div className="corner bottom-right landing-page-content">
-        <img
-          src="/image/hops-world.svg"
-          alt="hops-world"
-          className="game-logo image-glow"
-        />
-        <img
-          src="/image/hops-play.svg"
-          alt="hops-play"
-          className="game-logo image-glow landing-page-content"
-        />
-      </div>
-      <div className="corner top-right landing-page-content"></div>
     </motion.div>
   );
 };
